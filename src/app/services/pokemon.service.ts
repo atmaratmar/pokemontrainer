df346c748a354864d234c47802870d173eef5156
atmaratmar <atmar@hotmail.dk>
2021-05-08 09:27:28 +0200

    REFACT  changing the pokemon pic to card

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -10,26 +10,20 @@ export class PokemonsService{
 
   private _pokemon: Pokemon[] = []
     private _error : string='';
-
-
     constructor(private readonly http:HttpClient ){
-
     }
     getPokemons() {
-    return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=20`);
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=10`);
   }
 
   //get more data from pokemon
   getMoreData(name: string) {
     return this.http.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
   }
-
     public Pokemon():Pokemon[]{
         return this._pokemon;
     }
     public error(): string{
         return this._error
     };
-
-
 }
