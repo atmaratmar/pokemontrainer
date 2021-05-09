50e776e6660717cde8dae83f953ceb2b3b8ee0ca
atmaratmar <atmar@hotmail.dk>
2021-05-09 15:48:01 +0200

    change

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("/dist/pokemontrainer"));
+app.use(express.static(__dirname, "/../pokemontrainer"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join("/dist/pokemontrainer/index.html"));
+  res.sendFile(path.join(__dirname, "/../pokemontrainer/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
