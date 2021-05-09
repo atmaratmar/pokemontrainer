2df44c98f556d88f10271464c5bfbb0afdab72a2
atmaratmar <atmar@hotmail.dk>
2021-05-09 16:33:46 +0200

    dist add

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const path = require("path");
-
 const ngApp = express();
-
-ngApp.use(express.static("./"));
-
+ngApp.use(express.static("./dist/pokemontrainer"));
 ngApp.get("/*", function (request, response) {
-  response.sendFile(path.join(__dirname, "./index.html"));
+  response.sendFile(path.join(__dirname, "/dist/pokemontrainer/index.html"));
 });
-
 ngApp.listen(process.env.PORT || 8080);
